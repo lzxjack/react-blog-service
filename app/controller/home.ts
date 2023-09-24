5942f6b671cedcfc79df4cd5d925ed8a8b0720a7
Jack <965555169@qq.com>
2023-09-25 01:07:50 +0800

    update config

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -3,6 +3,10 @@ import { Controller } from 'egg';
 export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
-    ctx.body = 'hello world..';
+    ctx.body = {
+      name: 'egg',
+      category: 'framework',
+      language: 'Node.js'
+    };
   }
 }
--- config/config.default.ts
+++ config/config.default.ts
@@ -6,8 +6,18 @@ export type DefaultConfig = PowerPartial<EggAppConfig>;
 export default (appInfo: EggAppConfig) => {
   const config = {} as PowerPartial<EggAppConfig>;
 
-  // override config from framework / plugin
   config.keys = appInfo.name + '123456';
+
+  config.bodyParser = {
+    jsonLimit: '1mb',
+    formLimit: '1mb'
+  };
+
+  config.cookies = {
+    httpOnly: true,
+    sameSite: 'none',
+    secure: true
+  };
 
   return config;
 };
